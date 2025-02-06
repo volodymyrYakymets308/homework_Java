0b81f13e8402f2cab2b70b2a58869a56651476d5
volodymyrYakymets308 <yakymetsvolodymyr13@gmail.com>
2025-02-06 16:09:58 +0200

    Lessons_2

---
+++ src/A.java
@@ -0,0 +1,12 @@
+class A {
+    public String publicField = "Public";
+    protected String protectedField = "Protected";
+    String defaultField = "Default";
+    private String privateField = "Private";
+
+
+    public String getPrivateField() {
+        return privateField;
+    }
+}
+
---
+++ src/B.java
@@ -0,0 +1,12 @@
+class B {
+    public void printFields() {
+        A a = new A();
+
+
+        System.out.println("Public: " + a.publicField);
+        System.out.println("Protected: " + a.protectedField);
+        System.out.println("Default: " + a.defaultField);
+
+        System.out.println("Private: " + a.getPrivateField());
+    }
+}
